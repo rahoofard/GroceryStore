bbe70809f20683b2ddedbbbf8b4c6baebe5df079
rahoofard <rahoofard@mail.csuchico.edu>
2018-09-26 20:59:27 -0700

    Here I set up a grocery store menu that lets users input what goceries they want until the list gets too full with the ability to quit

--- main.cpp
+++ main.cpp
@@ -7,22 +7,39 @@ int main()
 {
   string list[5]; //array of 5 strings
   int numItems = 0;
-  char input;
+char input;
+string item;
 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
+ cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-do {
+ cout<<"\nYour choice (A/Q): ";
+ cin>>input;
+do{
+cout<<"What is your item?"<<endl;
+cin>>item;
+numItems = numItems+1;
+ 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
+ cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+ cout<<"\nYour choice (A/Q): ";
+ cin>>input;
+
 }
-while (input != 'Q');
-  return 0;
-}
+ while ((input == 'A' || input == 'a')&& numItems < 6 );
+
+cout<<"You'll need a bigger list!"<<endl;
+
+
+
+
+
+
+    
+  
+
+return 0;
+ }
